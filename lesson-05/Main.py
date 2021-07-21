e94a1ff254117608361a87c8d68a60eab13a3a1a
viktor <tmspython@gmail.com>
2021-07-21 21:01:00 +0300

    Done

---
+++ lesson-05/Main.py
@@ -0,0 +1,9 @@
+def format_string(name):
+    return f"Hello,  {name}"
+
+
+my_names = ['Alex', 'Oleg', 'Viktor', 'Roma', 'Egor']
+
+for name in my_names:
+    my_string = format_string(name)
+    print(my_string)
