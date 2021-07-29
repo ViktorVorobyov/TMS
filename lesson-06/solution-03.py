bab3dc33ecb3f011110e22a74b162ce3fc59926c
viktor <tmspython@gmail.com>
2021-07-29 17:21:01 +0300

    Correct list

--- lesson-06/solution-03.py
+++ lesson-06/solution-03.py
@@ -1,5 +1,3 @@
-"""Задачу решал в лоб и без оформления. Очень хотелось спать"""
-
 import random
 
 solar = random.randint(1, 1000000)
@@ -12,5 +10,5 @@ while solar > 0:
         x = x - 1
     elif solar // x ** 2 == 1:
         solar = solar - x ** 2
-        list.append(x)
+        list.append(x ** 2)
 print(list)
